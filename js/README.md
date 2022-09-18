722521710dd9f20863f128a0a1cd8613c1d2a80d
Jonathan Liera <jonliera1@gmail.com>
2022-09-18 16:35:35 -0700

    add function for the dice to roll and add numbers to the totalscore. A function where the player can hold their totalscore

--- js/README.md
+++ js/README.md
@@ -1,4 +1,18 @@
-Describe: Game   
+Describe: Game(p1, p2) 
 Test: It will store the players information
-Code: const game = new Game(p1, p2)
+Code: const game = new Game("p1", "p2")
+Expected Output: p1 and p2
+
+Describe: Player(name)
+Test: It will give the players name, turnscore, totalscore 
+Code: const player = new Player("name")
+Expected Output: Player's name 
+
+Test: It will change the turn score and the total score
+Code: 
 Expected Output:
+
+Describe playDice()
+Test: It will give a random number from 1-6.
+Code: playDice(1, 6)
+Expected Output: #s from 1-6
--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,31 @@ function Game(p1,p2){
   this.player2 = p2;
 }
 
+function Player(name){
+  this.playerName = name;
+  this.turnScore = 0;
+  this.totalScore = 0;
+}
+
+function playDice(min, max) {
+  min = Math.ceil(1);
+  max = Math.floor(6);
+  return Math.floor(Math.random() * (6 - 1 + 1) + 1);
+}
+
+Player.prototype.playDice = function(min, max){
+  min = Math.ceil(1);
+  max = Math.floor(6);
+  let output = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  if (output != 1) {
+    console.log(output);
+    return this.turnScore += output;
+  } else if (output=1) {
+    return this.turnScore = 0, this.totalScore = 0;
+  }
+}
+Player.prototype.holdDice = function(){
+  this.totalScore += this.turnScore;
+  this.turnScore = 0;
+  return this.totalScore += this.turnScore, this.turnScore = 0;
+}
