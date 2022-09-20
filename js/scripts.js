8568087389bb0e6de45d113c05a67e0c11daf370
Jonathan Liera <jonliera1@gmail.com>
2022-09-19 20:36:43 -0700

    add HTML and UI Logic for form submission

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,10 @@ function Player(name){
   this.totalScore = 0;
 }
 
-function playDice(min, max) {
-  min = Math.ceil(1);
-  max = Math.floor(6);
-  return Math.floor(Math.random() * (6 - 1 + 1) + 1);
-}
-
 Player.prototype.playDice = function(min, max){
   min = Math.ceil(1);
   max = Math.floor(6);
-  let output = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+    let output = Math.floor(Math.random() * (6 - 1 + 1) + 1);
   if (output != 1) {
     console.log(output);
     return this.turnScore += output;
@@ -26,8 +20,27 @@ Player.prototype.playDice = function(min, max){
     return this.turnScore = 0, this.totalScore = 0;
   }
 }
+
 Player.prototype.holdDice = function(){
   this.totalScore += this.turnScore;
   this.turnScore = 0;
   return this.totalScore += this.turnScore, this.turnScore = 0;
 }
+
+// UI Logic
+
+function handleFormSubmission(e) {
+  e.preventDefault();
+  const playerOne = document.getElementById("player1").value;
+  const playerTwo = document.getElementById("player2").value;
+  let player1 = new Player(playerOne);
+  let player2 = new Player(playerTwo);
+  let game = new Game(player1, player2);
+  console.log(game);
+}
+
+
+
+window.addEventListener("load", function() {
+  document.getElementById("game").addEventListener("submit", handleFormSubmission);
+})
